804cca1f9859c4176493602a50622c17b5da96ca
tntscreed <126970474+tntscreed@users.noreply.github.com>
2025-08-05 17:30:31 +0200

    Fixed some cmake issues

--- src/apps/configapp/configapp.cpp
+++ src/apps/configapp/configapp.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
 #include <imgui.h>
+#include <imgui_impl_sdl2.h>
+
+#ifdef _WIN32
+#include <Windows.h>
+#include <imgui_impl_dx9.h>
+#endif WIN32
 
 int main(){
-    std::cout << "Hello, ConfigApp!" << std::endl;
+    std::cout << "Hello, ConfigApp 2!" << std::endl;
     return 0;
 }
